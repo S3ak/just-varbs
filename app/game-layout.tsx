16bdfe081d0e1e0450096ecea14950b1dec6488c
Monde Sineke <nsineke@gmail.com>
2025-03-27 02:33:14 +0100

    refactor: :fire: remove duplicate footer

--- app/game-layout.tsx
+++ app/game-layout.tsx
@@ -9,9 +9,6 @@ export default function GameLayout({
     <div className="min-h-screen flex flex-col">
       <MainNav />
       <main className="flex-1 p-6 max-w-7xl mx-auto w-full">{children}</main>
-      <footer className="py-4 text-center text-gray-400 text-sm border-t border-gray-800">
-        © 2025 Just-Varbs. All rights reserved.
-      </footer>
     </div>
   );
 }
